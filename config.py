b4778cae044f4e85a050d51f336dd62066ae6298
tajamullone106-droid <tajamullone106@gmail.com>
2025-12-16 10:25:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", '30DxNexGenBotsc65acc') 
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsa54535') 
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
